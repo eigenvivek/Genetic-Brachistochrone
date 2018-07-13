8a604dbc087dad5f8964cb3907ea51bbf7fe6bfd
goatboy3million <vgopala4@jhu.edu>
2018-07-13 15:49:11 -0400

    Added seperate plotting function with cycloid

---
+++ src/genetics/curves.py
@@ -0,0 +1,46 @@
+import numpy as np
+from scipy import integrate
+from scipy.interpolate import interp1d
+from scipy.optimize import newton
+import matplotlib.pyplot as plt
+
+from genetics.constants import P1, P2
+
+# Linearly interpolate the two points
+def linear():
+    x = [p[0] for p in [P1, P2]]
+    y = [p[1] for p in [P1, P2]]
+    f_linear = interp1d(x, y, kind='linear')
+    def dt(x):
+        return np.sqrt((1 + ((P2[1] - P1[1]) / (P2[0] - P1[0]))**2) / (-f_linear(x)))
+    T = integrate.quad(dt, a=P1[0], b=P2[0], limit=10)[0]
+    return f_linear, T
+
+def cycloid():
+    """Return the path of Brachistochrone curve from (0,0) to (x2, y2).
+
+    The Brachistochrone curve is the path down which a bead will fall without
+    friction between two points in the least time (an arc of a cycloid).
+    It is returned as an array of N values of (x,y) between (0,0) and (x2,y2).
+
+    """
+
+    # First find theta2 from (x2, y2) numerically (by Newton-Rapheson).
+    def f(theta):
+        return -P2[1]/P2[0] - (1-np.cos(theta))/(theta-np.sin(theta))
+    theta2 = newton(f, np.pi/2)
+
+    # The radius of the circle generating the cycloid.
+    R = -P2[1] / (1 - np.cos(theta2))
+
+    theta = np.linspace(0, theta2, 100)
+    x = R * (theta - np.sin(theta))
+    y = R * (1 - np.cos(theta))
+
+    # The time of travel
+    T = theta2 * np.sqrt(R)
+    return x, -y, T
+
+"""Constants"""
+f_linear, T_linear = linear()
+x, y, T_cycloid = cycloid()
---
+++ src/genetics/visualize.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from genetics.curves import f_linear, T_linear, x, y, T_cycloid
+from genetics.constants import P1, P2
+
+def visualize(path, title=None):
+    """Returns a plot of the interpolated path."""
+
+    # A more granular domain
+    xnew = np.linspace(P1[0], P2[0], 1001)
+
+    # Plot setup
+    plt.plot(path.dna.x, path.dna.y, 'o')
+    plt.plot(xnew, path.dna.f(xnew), '--', label='Path')
+    plt.plot(xnew, [P1[1]] * len(xnew), label='Cutoff')
+    plt.plot(xnew, f_linear(xnew), label='Linear')
+    plt.plot(x, y, label='Cycloid')
+
+    # try:
+    #     plt.text(0.8, 0, str(path.time))
+    # except:
+    #     path.evaluate()
+    #     plt.text(0.8, 0, str(path.time))
+    # plt.text(0.8, -0.5, str(T))
+
+    # Plot titles
+    if title != None:
+        plt.title(title)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    # plt.legend()
+
+    # Render
+    plt.show()

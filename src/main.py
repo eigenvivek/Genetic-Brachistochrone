7b2297c4545302a6065e619dee29e7bf6917c47b
goatboy3million <vgopala4@jhu.edu>
2018-07-16 16:23:01 -0400

    updated main

--- src/main.py
+++ src/main.py
@@ -9,14 +9,14 @@ from genetics.visualize import visualize
 # test
 if __name__ == '__main__':
 
-pop = Population(100)
+    pop = Population(100)
 
-for generation in range(100):
-    pop.next_generation()
-    visualize(pop.generation_best)
+    for generation in range(100):
+        pop.next_generation()
+        visualize(pop.generation_best)
 
-    print("Generation: {} \nMax fitness: {} | Average fitness: {}".format(
-          generation, pop.generation_best.time, np.mean(pop.fitness))
-          )
+        print("Generation: {} \nMax fitness: {} | Average fitness: {}".format(
+              generation, pop.generation_best.time, np.mean(pop.fitness))
+              )
 
-    pop.hist_fitness_and_error()
+        pop.hist_fitness_and_error()

8b612a7d1057f8ff01e9742b576a5ce5e47a4543
v715 <vgopala4@jhu.edu>
2018-09-01 10:23:32 -0400

    new master file

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # Local moduless
 from genetics.population import Population
-# from genetics.visualize import visualize
+from genetics.visualize import visualize
 from genetics.curves import T_cycloid
 
 # test
@@ -14,8 +14,8 @@ if __name__ == '__main__':
 
     for generation in range(100):
         pop.next_generation()
-        # if generation % 10 == 0:
-        #     visualize(pop.generation_best)
+        if generation % 10 == 0:
+            visualize(pop.generation_best)
 
         print("Generation: {} \nBest time: {:.3f} | Average time: {:.3f} | Cycloid time: {:.3f}".format(
               generation, pop.generation_best.time, np.mean(pop.fitness), T_cycloid)
